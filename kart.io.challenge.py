4aa7fed45ffb412a3d99ce3f65dd60f555a1d82b
Jeff Silverman <jeffsilverm@gmail.com>
2016-01-08 00:21:13 -0800

    Passes all test cases

--- kart.io.challenge.py
+++ kart.io.challenge.py
@@ -14,31 +14,41 @@ def find_rectangle ( image ):
   
   print ("The size of the array is (%d, %d)" % (rows, cols) )
 
-  ul = 0
+  right = None
+  left = None
+  top = None
+  bottom = None
+# Search for the upper left corner
   for row in range(rows) :
     for col in range(cols) :
-      if image[row][col] == 0 
+      if image[row][col] == 0 and top == None:
 # The first thing we're going to come to the upper left corner, so save it
         ( top, left ) = (row, col)
-        for col_i in range(col,cols) :
+        for col in range(col,cols) :
 # We're now looping through the black rectangle: the upper right edge is when
 # the image turns white
-          if image[row][col_i] == 1 or col_i == cols-1 :
-            right = col_i
+          if image[row][col] == 1 :
+            right = col-1
+          elif col == cols-1 :   # we're at the right edge
+            right = col
+          if right != None :
             break
-# 
-          for row_i in range(row, rows) :
-            if image[row_i],[col_i] == 1 or row_i = rows-1 :
-              bottom = row_1
-              break
+        for row in range(row, rows) :
+          if image[row][left] == 1 :
+            bottom = row-1
+          elif  row == rows-1 :
+            bottom = row
+          if bottom != None :
+            break
+        break  # We don't have to search for the upper left corner
+  return ( top, left, bottom, right )
+        
 
 
 
 
-# go to the next row
 
 
-  return (( ul[0],ul[1],ll[0],ur[1]) )
 
 
 
@@ -46,6 +56,22 @@ def find_rectangle ( image ):
 if __name__ == "__main__" :
   
 
+  def test ( image, top, left, bottom, right ) :
+    """This subroutine tests the image against the 4 nominal values, and throws
+assertion errors if the nominal val
+ue is wrong"""
+
+
+    (t,l,b,r) = find_rectangle( image )
+    assert t == top, "Upper row is %d, should be %d" % (t, top)
+    assert l == left, "left colum is %d, should be %d" % (l, left)
+    assert b == bottom, "Lower row is %d, should be %d" % (b, bottom )
+    assert r == right, "right column is %d, should be %d" % (r, right)
+# Computers start counting at 0, but humans start counting at 1
+    print("The black rectangle is at (%d,%d) to (%d,%d)" % ( t+1, l+1, b+1, r+1 ))
+
+
+# This is the image given in the problem statement
   image = np.array ([
 	[1, 1, 1, 1, 1, 1, 1],
 	[1, 1, 1, 1, 1, 1, 1],
@@ -53,13 +79,9 @@ if __name__ == "__main__" :
 	[1, 1, 1, 0, 0, 0, 1],
 	[1, 1, 1, 1, 1, 1, 1],
 ])
-  (ur,lc,lr,rc) = find_rectangle( image )
-  assert ur == 2, "Upper row is %d, should be 2" % ur
-  assert lc == 3, "left colum is %d, should be 3" % lc
-  assert lr == 3, "Lower row is %d, should be 3" % lr
-  assert rc == 6, "right column is %d, should be 6" % lc
-# Computers start counting at 0, but humans start counting at 1
-  print("The black rectangle is at (%d,%d) to (%d,%d)" % ( ur+1, lc+1, lr+1, rc+1 ))
+  
+  print ("Given image from the problem statement")
+  test ( image, 2, 3, 3, 5 )
 
 # Another test case: rectangle at the right edge
   image = np.array ([
@@ -69,13 +91,30 @@ if __name__ == "__main__" :
 	[1, 1, 1, 0, 0, 0, 0],
 	[1, 1, 1, 1, 1, 1, 1],
 ])
-  (ur,lc,lr,rc) = find_rectangle( image )
-  assert ur == 2, "Upper row is %d, should be 2" % ur
-  assert lc == 3, "left colum is %d, should be 3" % lc
-  assert lr == 3, "Lower row is %d, should be 3" % lr
-  assert rc == 7, "right column is %d, should be 6" % lc
-# Computers start counting at 0, but humans start counting at 1
-  print("The black rectangle is at (%d,%d) to (%d,%d)" % ( ur+1, lc+1, lr+1, rc+1 ))
+  print ("Rectangle at the right edge")
+  test ( image, 2, 3, 3, 6 )
+ 
+# Another test case: rectangle at the left edge
+  image = np.array ([
+	[1, 1, 1, 1, 1, 1, 1],
+	[1, 1, 1, 1, 1, 1, 1],
+	[0, 0, 0, 0, 0, 0, 1],
+	[0, 0, 0, 0, 0, 0, 1],
+	[1, 1, 1, 1, 1, 1, 1],
+])
+  print ("Rectangle at the left edge")
+  test ( image, 2, 0, 3, 5)
 
+# Another test case: rectangle at the bottom edge
+  image = np.array ([
+	[1, 1, 1, 1, 1, 1, 1],
+	[1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 1],
+])
+  print ("Rectangle at the bottom")
+  test ( image, 2, 1, 4, 5)
 
+  
 
