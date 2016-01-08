fa940c47996a4643d9b54e9bf516816dc50dafac
Jeff Silverman <jeffsilverm@gmail.com>
2016-01-08 00:38:49 -0800

    Second part

---
+++ kart.io.challenge_2.py
@@ -0,0 +1,25 @@
+
+# Now let's assume there are N solid black rectangles in the image. Find them
+# all and return their coordinates. For example:
+
+
+
+
+
+
+
+  image = [
+	[1, 1, 1, 1, 1, 1, 1],
+	[1, 1, 1, 1, 1, 1, 1],
+	[1, 1, 1, 0, 0, 0, 1],
+	[1, 0, 1, 0, 0, 0, 1],
+	[1, 0, 1, 1, 1, 1, 1],
+	[1, 0, 1, 0, 0, 1, 1],
+	[1, 1, 1, 0, 0, 1, 1],
+	[1, 1, 1, 1, 1, 1, 1],
+ ];
+  rectangles = [(2,3,3,5),
+  def test ( image, 
+
+    
+
