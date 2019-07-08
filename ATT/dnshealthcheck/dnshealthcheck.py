e6b82106b46f6714e0c70d76f274d6a1b942c17e
Jeffrey Silverman <jsilverman@juniper.net>
2019-07-08 00:32:37 -0700

    It runs and mostly works The comparison of the IPv6 address as returned and the IPv6 address that is nominal does not work properly yet. Also, test that a host with a list of IPv4 addresses or  list of IPv6 addresses works properly.

--- ATT/dnshealthcheck/dnshealthcheck.py
+++ ATT/dnshealthcheck/dnshealthcheck.py
@@ -20,6 +20,8 @@ from typing import List, Union
 # As of 5-July-2019, it was version 3.2.0
 import DNS
 
+# Google's public name servers are 8.8.8.8 and 8.8.4.4 for IPv4, and
+# 2001:4860:4860::8888 and 2001:4860:4860::8844 for IPv6.
 reqobj = DNS.Request(server="192.168.0.1")
 
 
@@ -64,30 +66,56 @@ def verify_nameserver(ns_ut: Union[dict, list]) -> None:
                          f"but got a {str(type(ns_ut))} instead")
 
 
+def extract_list_of_data(ans: DNS.DnsResult) -> List:
+    """
+
+    :param ans: a DnsResult object from a call to DNS.req
+    :return: a list of data from the ans object
+    """
+    data_list = []
+    for a in ans.answers:
+        data_list.append(a['data'])
+    return data_list
+
+
 def verify_nameserver_call(key: dict) -> None:
     """This method is what actually does the heavy lifting.
     It breaks appart the key to get
 
     """
     nameserver = key["nameserver"]
-    qtype = key["qtype"]
+    qtype = key.get("qtype", "A")
     name = key["name"]
     nominal = key["nominal"]
-    answer = reqobj.req(qtype=qtype, server=nameserver, name=name)
-    print(dir(answer), nominal)
-    failure = answer.status == "NXDOMAIN"  # This is wrong.  Failure should
-    # be when the success string is not seen
-    if failure:
-        log_fail(nameserver, qtype, name, answer.status)
+    comment = key.get("comment", "")
+    try:
+        answer = reqobj.req(qtype=qtype, server=nameserver, name=name)
+    except DNS.Base.TimeoutError as d:
+        log_fail(nameserver, qtype, name, f"Timed out: {str(d)}", comment)
+    else:
+        data_list: list = extract_list_of_data(answer)
+        failure = answer.header['status'] != "NOERROR"
+        if failure:
+            log_fail(nameserver, qtype, name, answer.header['status'], comment)
+        elif nominal not in data_list:
+            log_fail(nameserver, qtype, name, f"'{nominal} not in {data_list}'",
+                     comment)
+        else:
+            log_success(nameserver, qtype, name, data_list, comment)
 
 
-def log_fail(nameserver, qtype, name, status) -> None:
+def log_fail(nameserver, qtype, name, status, comment="") -> None:
     print(f"nameserver {nameserver} FAILED to lookup {qtype} of {name} "
-          f"status is {status}")
+          f"status is {status} " + comment)
+
+
+def log_success(nameserver, qtype, name, d_lst, comment="") -> None:
+    print(f"nameserver {nameserver} successfully looked up {qtype} of {name}"
+          f" and got {d_lst} " + comment)
 
 
 if "__main__" == __name__:
-    config_filename = "dns_demo_01.json"
+    config_filename = sys.argv[1]
     while True:
         try:
             main(config_filename)
