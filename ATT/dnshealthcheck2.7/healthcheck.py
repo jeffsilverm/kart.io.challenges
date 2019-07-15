86f17b9190243bea2887fb754bf724b0a0ade445
Jeffrey Silverman <jsilverman@juniper.net>
2019-07-14 22:40:43 -0700

    Making progress.
    Passes all test cases, including all intentional failures.  I'm not
    sure how to induce NTP to fail.
    
    Actually, I do.  Block UDP port 123 at the firewall.

--- ATT/dnshealthcheck2.7/healthcheck.py
+++ ATT/dnshealthcheck2.7/healthcheck.py
@@ -5,11 +5,16 @@
 #
 from __future__ import print_function
 
+# The following line comes from
+# https://gist.github.com/pylover/7870c235867cf22817ac5b096defb768
+# noinspection PyCompatibility
+import builtins  # noqa
 import datetime
 import os
 import stat
 import subprocess
 import sys
+import traceback
 
 # If the following import fails, then use pip to install package future
 
@@ -23,7 +28,8 @@ class Logger(object):
     """This class implements a simple logger """
 
     def __init__(self, logfile, hostname):
-        assert isinstance(logfile, file), "logfile should be type 'file' but" \
+        assert isinstance(logfile,
+                          builtins.file), "logfile should be type 'file' but" \
                                           "but is really %s" % str(
             type(logfile))
         self.logfile = logfile
@@ -54,30 +60,40 @@ class Checker(object):
         # a message
         # and the parameters
         self.report_dict = dict()
+        self.lc = 0
+        self.comment = ""
 
-    def append(self, key, err_level, message):
+    def append(self, key, err_level, message) -> None:
         """
 
         :rtype: None
         """
         entry = (err_level, message)
         if key in self.report_dict:
-            self.report_dict[key] = [entry]
-        else:
             self.report_dict[key].append(entry)
+        else:
+            self.report_dict[key] = [entry]
+
+    def record_lc(self, lc, comment=""):
+        """This records the instruction file line counter and the comment"""
+        self.lc = lc
+        self.comment = comment
 
     def logit(self, inst, status, command, message, results) -> None:
         error_level_str = \
             ("SUCCESS: " if status == 0 else (
                 "WARNING: " if status == WARNING else "FAIL:"))  # type: str
-        # results is a byte array and it does not render \n properly.
-        results_str = results.decode("ascii")
+        if isinstance(results, str):
+            results_str = results
+        else:
+            # results is a byte array and it does not render \n properly.
+            results_str = results.decode("ascii")
         print(
-            "*** {}`` {}``  {}``  ({})`` ".format(
+            "*** ``{}`` {}``  {}``  ({}:{:d})`` ".format(
                 inst, error_level_str,
-                self.hostname, self.inst_filename,
+                self.hostname, self.inst_filename, self.lc,
                 inst_lc), 50 * "*", "\n>>> %s " % " ".join(command),
-            "\n", message, "\n", file=self.logfile)
+            "\n", message, "\n", self.comment, file=self.logfile)
         print(results_str, file=self.logfile)
         self.logfile.flush()
 
@@ -90,14 +106,18 @@ class Checker(object):
         if params[0] != "" and params[0] is not None:
             args.append("@" + params[0])
         args.append(params[1])  # target name
-        args.extend(["-t", params[2]])  # qtype
+        if len(params) > 2:
+            args.extend(["-t", params[2]])  # qtype
+        else:
+            args.extend(["-t", "A"])
         try:
             results = subprocess.check_output(args)
         except subprocess.CalledProcessError as p:
             status = p.returncode
             results = ""
             message = "FAIL: dns check failed because " \
-                      "subprocess.check_output raised a %s exception" % str(p)
+                      "subprocess.check_output raised a {} exception".format(
+                         str(p))
         else:
             status = 0
             message = "PASS"
@@ -141,22 +161,39 @@ class Checker(object):
         self.logit(inst="NTP", status=status, message=message, results=results,
                    command=args)
 
+    @staticmethod
+    def get_file_attrs(filename):
+        """This function returns the uid, gid, and mode of the file
+        :type filename: str
+        :returns    tuple   int, int, int   UID, GID, mode
+        """
+
+        st = os.stat(filename)
+        uid = st.st_uid
+        gid = st.st_gid
+        mode = stat.S_IMODE(st.st_mode)
+        return uid, gid, mode
+
     def check_file(self, params):
         """Check that a file is protected properly and is owned properly"""
-        nom_uid = (int(params[1]) if isinstance(params[1], str) else params[1])
-        nom_gid = (int(params[2]) if isinstance(params[2], str) else params[2])
-        nom_mode = (int(params[3], base=8) if isinstance(params[3], str) else
-                    params[2])
-        results = b""
+
+        results = b""  # Since this routine doesn't invoke any commands. there
+        # no results
+        args = [""]
+
         try:
-            st = os.stat(params[0])
+            (uid, gid, mode) = self.get_file_attrs(params[0])
         except Exception as e:
             status = 1
             message = "FAIL: os.stat method raised an exception %s" % str(e)
         else:
-            uid = st.st_uid
-            gid = st.st_gid
-            mode = stat.S_IMODE(st.st_mode)
+            nom_uid = (
+                int(params[1]) if isinstance(params[1], str) else params[1])
+            nom_gid = (
+                int(params[2]) if isinstance(params[2], str) else params[2])
+            nom_mode = (
+                int(params[3], base=8) if isinstance(params[3], str) else
+                params[2])
             status = 0
             if uid == params[1]:
                 uid_ok = "UID OK %d " % uid
@@ -176,12 +213,52 @@ class Checker(object):
                 # humans to see ints in oct
                 mode_ok = "MODE OK"
             else:
-                mode_ok = "WRONG: mode is %o should be %s " % (mode,
-                                                               oct(params[3]))
+                mode_ok = "WRONG: mode is %o should be %o " % (mode, nom_mode)
                 status = WARNING
             message = uid_ok + gid_ok + mode_ok
-        self.logit(inst="FILE", status=status, command="",
+        self.logit(inst="FILE", status=status, command=args,
                    message=message, results=results)
+        return status, message
+
+    def check_erc(self, params):
+        """This checks the file /etc/resolv.conf """
+
+        # I decided to test /etc/resolv.conf as a file in a separate test
+        # (  status, message ) = self.check_file(params)
+        message = "FAIL: bad %s file: no 'nameserver' lines were found" % \
+                  params[0]
+        try:
+            ercf = open(params[0], "r")
+            lines = ercf.readlines()
+            ercf.close()
+        except Exception as e:
+            message = "FAIL: reading the file %s raised an exception %s" % \
+                      (params[0], str(e))
+            self.logit(inst="ERC", status=0,
+                       command=[],
+                       message=message,
+                       results=b"")
+        else:
+            for l in lines:
+                elements = l.split()
+                if len(elements) > 0 and "nameserver" == elements[0]:
+                    ipaddr = elements[1]
+                    message = "DNS test on server %s with host %s" % \
+                              (ipaddr, params[2])
+                    self.logit(inst="ERC", status=0,
+                               command=['dig', '@' + ipaddr, params[2]],
+                               message=message,
+                               results=b"See below")
+                    self.check_dns(params)
+
+            # There is a fundamental design flaw in this program I am not
+            # going to fix now - the check_XXX methods should return the test
+            # results so that one method can call another.
+        if "FAIL" in message:
+            status = 1
+        else:
+            status = 0
+        return status
 
     def check_snmp(self, cn, params):
         """This method checks SNMP"""
@@ -202,7 +279,7 @@ class Checker(object):
             message = WIAR
             error_level = "PASS"
         # hide the community name string
-        args[3] = "XXXX"
+        args[3] = "XXXX"  # str
 
         self.logit(inst="SNMP", status=status, command=args,
                    message=message, results=results)
@@ -237,6 +314,10 @@ class Checker(object):
                 assert test in self.report_generator()
                 raise NotImplemented
 
+    def close_log_file(self):
+        print("ENDENDENDEND\n", file=self.logfile)
+        self.logfile.close()
+
 
 def get_my_fqdn():
     """Return the fully qualified domain name of this machine."""
@@ -258,21 +339,42 @@ def main(args):
     inst_file = open(args[1], "r")
     checker = Checker(logger_filename=sys.argv[2], inst_filename=sys.argv[1],
                       my_fqdn=my_fqdn)
-
-    for instruction in inst_file.readlines():  # type: str
-        parameters = instruction.rstrip().split("\t")
-        inst = parameters.pop(0)
-        if inst == "DNS":
-            checker.check_dns(parameters)
-        elif inst == "PING":
-            checker.check_ping(parameters)
-        elif inst == "NTP":
-            checker.check_ntp(parameters)
-        elif inst == "SNMP":
-            checker.check_snmp(COMMUNITY_NAME, parameters)
-        else:
-            print("bad instruction: %s, continuing" % inst, file=sys.stderr)
-        inst_lc += 1
+    try:
+        for instruction in inst_file.readlines():  # type: str
+            if instruction == "\n":
+                print("Came to an empty line in %s" % sys.argv[1],
+                      file=sys.stderr)
+                continue
+            parameters = instruction.rstrip().split("\t")
+            # Is there a comment in the last column?  It begins with # if so
+            if parameters[-1][0] == "#":
+                comment = parameters[-1]
+                parameters = parameters[0:-1]
+            else:
+                comment = ""
+            checker.record_lc(inst_lc, comment)
+            inst = parameters.pop(0)
+            if inst == "DNS":
+                checker.check_dns(parameters)
+            elif inst == "PING":
+                checker.check_ping(parameters)
+            elif inst == "NTP":
+                checker.check_ntp(parameters)
+            elif inst == "SNMP":
+                checker.check_snmp(COMMUNITY_NAME, parameters)
+            elif inst == "FILE":
+                checker.check_file(parameters)
+            elif inst == "ERC":
+                checker.check_erc(parameters)
+            else:
+                print("bad instruction: %s, continuing" % inst, file=sys.stderr)
+            inst_lc += 1
+        checker.close_log_file()
+    except Exception as e:
+        print("Raised an exception at line %d in the instruction file\n" %
+              checker.lc, "comment is %s\n" % checker.comment,
+              str(e), traceback.print_exc(),
+              file=sys.stderr)
 
 
 if "__main__" == __name__:
